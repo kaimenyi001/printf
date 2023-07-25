4eb9eee67d850a5a3bbff0f02386bad722d4f835
kaimenyi001 <133211709+kaimenyi001@users.noreply.github.com>
2023-07-25 22:02:11 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -10,10 +10,10 @@ int _printf(const char *format, ...)
 	int (*fun)(va_list, char *, unsigned int);
 	unsigned int a = 0, len = 0, abuf = 0;
 	va_list argts;
-	char *bffr;
+	char *buffer;
 
-	va_start(argts, format), bffr = malloc(sizeof(char) * 1024);
-	if (!format || !bffr || (format[a] == '%' && !format[a + 1]))
+	va_start(argts, format), buffer = malloc(sizeof(char) * 1024);
+	if (!format || !buffer || (format[a] == '%' && !format[a + 1]))
 		return (-1);
 	if (!format[a])
 		return (0);
@@ -23,7 +23,7 @@ int _printf(const char *format, ...)
 		{
 			if (format[a + 1] == '\0')
 			{
-				print_buf(bffr, abuf), free(bffr), va_end(argts);
+				print_buf(buffer, abuf), free(buffer), va_end(argts);
 				return (-1);
 			}
 			else
@@ -33,20 +33,20 @@ int _printf(const char *format, ...)
 				{
 					if (format[a + 1] == ' ' && !format[a + 2])
 						return (-1);
-					handl_buf(bffr, format[a], abuf), len++, a--;
+					handl_buf(buffer, format[a], abuf), len++, a--;
 				}
 				else
 				{
-					len += fun(argts, bffr, abuf);
+					len += fun(argts, buffer, abuf);
 					a += ev_print_func(format, a + 1);
 				}
 			} a++;
 		}
 		else
-			handl_buf(bffr, format[a], abuf), len++;
+			handl_buf(buffer, format[a], abuf), len++;
 		for (abuf = len; abuf > 1024; abuf -= 1024)
 			;
 	}
-	print_buf(bffr, abuf), free(bffr), va_end(argts);
+	print_buf(buffer, abuf), free(buffer), va_end(argts);
 	return (len);
 }
