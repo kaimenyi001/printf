3e858a92813d44b0a28802947e8a5ab6d2c146ca
kaimenyi001 <deniskaimenyi1@gmail.com>
2023-07-25 06:49:30 -0700

    printf

---
+++ handl_buf.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * handl_buf - concats characters
+ * @buf: buffer pointer
+ * @c: character to concat
+ * @abuf: index of buffer pointer
+ * Return: void
+ */
+unsigned int handl_buf(char *buf, char c, unsigned int ibuf)
+{
+	if (abuf == 1024)
+	{
+		print_buf(buf, abuf);
+		abuf = 0;
+	}
+	buf[abuf] = c;
+	abuf++;
+	return (abuf);
+}
