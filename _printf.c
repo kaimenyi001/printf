975cea10b6eab63d729ff4ca285b28275e9cd7b1
wintubu <bunwinny@gmail.com>
2023-07-25 11:55:54 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
  * _printf - Printf function
  * @format: format.
@@ -7,46 +9,59 @@
  */
 int _printf(const char *format, ...)
 {
-	int (*fun)(va_list, char *, unsigned int);
-	unsigned int a = 0, ln = 0, abuf = 0;
-	va_list argts;
-	char *bffr;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-	va_start(argts, format), bffr = malloc(sizeof(char) * 1024);
-	if (!format || !bffr || (format[a] == '%' && !format[a + 1]))
+	if (format == NULL)
 		return (-1);
-	if (!format[a])
-		return (0);
-	for (a = 0; format && format[a]; a++)
+
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (format[a] == '%')
+		if (format[i] != '%')
 		{
-			if (format[a + 1] == '\0')
-			{
-				print_buf(bffr, abuf), free(bffr), va_end(argts);
-				return (-1);
-			}
-			else
-			{
-				fun = get_print_func(format, a + 1);
-				if (fun == NULL)
-				{
-					if (format[a + 1] == ' ' && !format[a + 2])
-						return (-1);
-					handl_buf(bffr, format[a], abuf), ln++, a--;
-				}
-				else
-				{
-					ln += fun(argts, bffr, abuf);
-					a += ev_print_func(format, a + 1);
-				}
-			} a++;
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
 		else
-			handl_buf(bffr, format[a], abuf), ln++;
-		for (abuf = ln; abuf > 1024; abuf -= 1024)
-			;
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
 	}
-	print_buf(bffr, abuf), free(bffr), va_end(argts);
-	return (ln);
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
 }
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
+
